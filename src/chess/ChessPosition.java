c8638e303adcbdd035c9c8a5631f3f43304d66a3
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-10 17:41:08 -0300

    Check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -11,7 +11,7 @@ public class ChessPosition {
 
 		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 
-			throw new ChessException("Error instantiating ChessPorition. Valid values are from a1 to h8.");
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 
 		}
 		this.column = column;
@@ -31,7 +31,7 @@ public class ChessPosition {
 	}
 	
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition( (char)('a' - position.getCollumn()), 8 - position.getRow());
+		return new ChessPosition( (char)('a' + position.getCollumn()), 8 - position.getRow());
 	}
 	@Override
 	
