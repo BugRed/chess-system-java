76207f68e8ba1057de664a0b38b6af871286edce
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-13 16:40:26 -0300

    Checkmate Logic

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -4,32 +4,27 @@ public abstract class Piece {
 
 	protected Position position;
 	private Board board;
-
-	public Piece() {
-
-	}
-
+	
 	public Piece(Board board) {
 		this.board = board;
+		position = null;
 	}
 
 	protected Board getBoard() {
 		return board;
 	}
-
+	
 	public abstract boolean[][] possibleMoves();
-
+	
 	public boolean possibleMove(Position position) {
-		return possibleMoves()[position.getRow()][position.getCollumn()];
-
+		return possibleMoves()[position.getRow()][position.getColumn()];
 	}
-
+	
 	public boolean isThereAnyPossibleMove() {
-
-		boolean[][] matz = possibleMoves();
-		for (int i = 0; i < matz.length; i++) {
-			for (int j = 0; j < matz.length; j++) {
-				if (matz[i][j]) {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
 					return true;
 				}
 			}
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -2,41 +2,37 @@ package boardgame;
 
 public class Position {
 
-	private Integer row;
-	private Integer collumn;
-
-	public Position() {
-
-	}
-
-	public Position(Integer row, Integer collumn) {
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
 		this.row = row;
-		this.collumn = collumn;
+		this.column = column;
 	}
 
-	public Integer getRow() {
+	public int getRow() {
 		return row;
 	}
 
-	public Integer getCollumn() {
-		return collumn;
-	}
-
-	public void setRow(Integer row) {
+	public void setRow(int row) {
 		this.row = row;
 	}
 
-	public void setCollumn(Integer collumn) {
-		this.collumn = collumn;
+	public int getColumn() {
+		return column;
 	}
 
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
 	public void setValues(int row, int column) {
 		this.row = row;
-		this.collumn = column;
+		this.column = column;
 	}
-
+	
 	@Override
 	public String toString() {
-		return row + ", " + collumn;
+		return row + ", " + column;
 	}
 }
