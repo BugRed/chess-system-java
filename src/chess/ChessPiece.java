98b98f5af8a97e9a3224c026910e42e049af3d31
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-14 20:10:41 -0300

    fix - increase and decreace count protected

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -21,11 +21,11 @@ public abstract class ChessPiece extends Piece {
 	public int getMoveCount() {
 		return moveCount;
 	}
-	public void increaseMoveCount() {
+	protected void increaseMoveCount() {
 		moveCount++;
 	}
 	
-	public void decreaseMoveCount() {
+	protected void decreaseMoveCount() {
 		moveCount--;
 	}
 
